ce6c5fd32f307d7b393794bbdea54f7f18423100
DM <dm@DMs-MacBook-Pro.fios-router.home>
2020-01-16 18:44:28 -0500

    messed up class syntax. fixed, and added get item function

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
-class Item(self, item_name, item_description):
+class Item:
+    def __init__(self, item_name, item_description)
     self.item_name = item_name
     self.item_description = item_description
+
+    def get_item():
+        print(f'You picked up {self.item_name}!')
